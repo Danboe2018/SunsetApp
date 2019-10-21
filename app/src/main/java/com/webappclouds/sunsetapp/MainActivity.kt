2af62aa74d4df6c565a3e5356bf9a70ca67e554c
Dan Boehmke <Daniel@webappclouds.com>
2019-10-21 16:09:57 -0400

    Creates layout

--- app/src/main/java/com/webappclouds/sunsetapp/MainActivity.kt
+++ app/src/main/java/com/webappclouds/sunsetapp/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.webappclouds.sunsetapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    protected fun getSunset(view: View) {
+        var city = etCityName.text.toString()
+        val url =
+            "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22" + city + "%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys"
     }
 }
